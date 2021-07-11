19597942da06dcd7c01aa064283780d62a90174c
Al-Dabbagh Harith <messi4ever500@gmail.com>
2021-07-11 11:17:31 +0300

    Renamed The class

---
+++ USDtoRUB.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class USDtoRUB:
+
+    rates = {}
+
+    def __init__(self):
+        f = open("key.apikey", "r")  # .apikey file contains the APP ID from Open Exchange Rates
+        key = f.read().strip()
+        f.close()
+        url = "https://openexchangerates.org/api/latest.json?app_id=" + key
+        data = requests.get(url)
+        data = data.json()
+        self.rates = data["rates"]
+
+    def getUSDtoRUB(self):
+        return self.rates["RUB"]
+
+
+converter = USDtoRUB()
+
+print(round(converter.getUSDtoRUB(), 2))
